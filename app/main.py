0c10610d825f323c6d82e6e9ded4510ed2f15db5
adisacodes <marian.adisa@student.moringaschool.com>
2026-06-16 14:41:22 +0300

    render deployment

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+
 from app.routers import auth, donors, requests, admin
 from app.database import engine
 from app.models import models
+from app.utils.seed_admin import seed_super_admin
 
+# Create tables
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Blood Donation Management System")
@@ -11,12 +15,13 @@ app = FastAPI(title="Blood Donation Management System")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173", 
+        "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5174",
         "http://localhost:5175",
 
+        "https://blood-donations-frontend.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
@@ -31,11 +36,6 @@ app.include_router(admin.router, prefix="/api/admin", tags=["Admin"])
 @app.get("/")
 def root():
     return {"message": "Welcome to Blood Donation API 🩸"}
-
-# Seed super admin on startup
-from app.database import engine
-from app.utils.seed_admin import seed_super_admin
-from sqlalchemy.orm import Session
 
 @app.on_event("startup")
 def seed_admin_on_startup():
